da4e80d95babf2eed67666a63d84c535c41a0466
DangKhoa2003 <tranhadangkhoa2003@gmail.com>
2022-12-23 17:03:21 +0700

    Route Link

--- app/src/main.js
+++ app/src/main.js
@@ -10,9 +10,7 @@ import './assets/css/magnific-popup.css';
 import './assets/css/flaticon.css';
 import './assets/css/style.css';
 
-global.jQuery = require('jquery');
-var $ = global.jQuery;
-window.$ = $;
+
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import '@/assets/js/owl.carousel.min.js';
 import '@/assets/js/jquery-migrate-3.0.1.min.js';
--- app/src/router/index.js
+++ app/src/router/index.js
@@ -14,7 +14,40 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
+  {
+    path: '/products',
+    name: 'products',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('../views/ProductsView.vue')
+  },
+  {
+    path: '/blog',
+    name: 'blog',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('../views/BlogView.vue')
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('../views/ContactView.vue')
+  },
+
+  {
+    path: '/cart',
+    name: 'cart',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('../views/CartView.vue')
+  },
 ]
 
 const router = createRouter({
